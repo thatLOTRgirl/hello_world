d907ccdd9f163c6cd763b5e7435b60b23e3526d6
Julia <jchristensen9948@gmail.com>
2023-11-01 18:40:57 -0400

    Last Change on Hello World

--- hello.java
+++ hello.java
@@ -8,7 +8,7 @@ public class hello {
        String firstName = "Julia";
 
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
-       System.out.println("Hello, i am " + firstName + ".");
+       System.out.println("Hello, I am " + firstName + ".");
     }
 }
 
